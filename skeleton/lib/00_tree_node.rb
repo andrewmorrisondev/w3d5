63dc223ccbdd11d63e14e5f356295c3bb0a2d229
andy <morrison.andrew422@gmail.com>
2023-01-27 16:04:32 -0500

    not much tbh but high hopes

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -11,16 +11,16 @@ class PolyTreeNode
     end
 
     def parent=(new_parent)
-        debugger
-        if parent != nil # adoptive
+        # debugger
+        if parent != nil # adoption - remove old parent, add new parent
             parent.children.delete(self)
-            parent = new_parent
+            @parent = new_parent
             parent.children << self if !parent.children.include?(self)
-        elsif parent != nil && new_parent == nil
+        elsif parent != nil && new_parent == nil # orpan - remove parent
             parent.children.delete(self)
-            parent = new_parent
-        else
-            parent = new_parent
+            @parent = new_parent
+        else  # emancipation - 
+            @parent = new_parent
             parent.children << self if !parent.children.include?(self)
         end
     end
