f86665ccfab4ec954662bf059430fc4a4f6dc08b
“Kelsang” <kelsangrtsering@gmail.com>
2023-01-27 15:30:31 -0500

    Work further on parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,6 +1,8 @@
+require "byebug"
+
 class PolyTreeNode
-    attr_reader :value
-    attr_accessor :parent, :children
+    attr_reader :parent, :children, :value
+    attr_writer :parent, :children
 
     def initialize(value)
         @parent = nil
@@ -9,12 +11,18 @@ class PolyTreeNode
     end
 
     def parent=(new_parent)
-        if self.parent != nil # adoptive
-            self.parent.children.delete(self)
-            self.parent = new_parent
+        debugger
+        if parent != nil # adoptive
+            parent.children.delete(self)
+            parent = new_parent
+            parent.children << self if !parent.children.include?(self)
+        elsif parent != nil && new_parent == nil
+            parent.children.delete(self)
+            parent = new_parent
+        else
+            parent = new_parent
+            parent.children << self if !parent.children.include?(self)
         end
-        self.parent = node
-        node.children << self if node != nil
     end
 
     
