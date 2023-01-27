b07cdd70eddbbb2ecb107ac8e07fa84a5667ffa1
“Kelsang” <kelsangrtsering@gmail.com>
2023-01-27 14:11:05 -0500

    Initialize PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
 
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(new_value)
+        parent = new_value
+        parent.children << new_value if parent != nil
+    end
 end
