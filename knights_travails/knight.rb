35f399276193165f2bffedab02c4c40d2265584f
andy <morrison.andrew422@gmail.com>
2023-01-30 14:19:16 -0500

    fix self.valid_moves

--- knights_travails/knight.rb
+++ knights_travails/knight.rb
@@ -1,10 +1,6 @@
 require_relative './skeleton/lib/00_tree_node.rb'
 
 class KnightPathFinder
-  
-  def build_move_tree
-    
-  end
 
   def initialize(pos)
     @considered_positions = [pos]
@@ -13,6 +9,7 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos) # [3,3]
+    valid_moves_arr = []
     move_equations = [[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [-2, 1], [2, -1], [2, 1]]
     move_equations.each do |equation|
       pos_x = pos[0]
@@ -20,23 +17,26 @@ class KnightPathFinder
       eq_x = equation[0]
       eq_y = equation[1]
 
-      if (pos_x + eq_x <= 7 && pox_x + eq_x >= 0) && (pos_y + eq_y <= 7 && pox_y + eq_y >= 0)
-        return true
-        
-      else  
-        return false 
+      if (pos_x + eq_x <= 7 && pos_x + eq_x >= 0) && (pos_y + eq_y <= 7 && pox_y + eq_y >= 0)
+        valid_moves_arr << [pos_x + eq_x, pos_y + eq_y]
       end
-        # << PolyTreeNode.new([pos_x + eq_x, pos_y + eq_y])
-
+      valid_moves_arr
     end
   end
 
   def new_move_positions(pos)
     if !@considered_positions.include?(pos)
-      self.valid_moves(pos)
-      @considered_positions << pos
-      return pos
+      if valid_moves(pos)
+        @considered_positions << pos
+        return pos
+      end
     end
+  end
+
+  def build_move_tree
+    queue = [Node.new(start_pos)]
+    new_move_positions(queue.shift)
+
   end
 
 
