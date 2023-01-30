e8be0b798740a5fbd1d44bbf80a65128d961c02b
“Kelsang” <kelsangrtsering@gmail.com>
2023-01-30 14:41:06 -0500

    Modify new_move_positions(pos) method

--- knights_travails/knight.rb
+++ knights_travails/knight.rb
@@ -10,27 +10,42 @@ class KnightPathFinder
 
   def self.valid_moves(pos) # [3,3]
     valid_moves_arr = []
-    move_equations = [[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [-2, 1], [2, -1], [2, 1]]
+    move_equations = [
+      [-1, -2], 
+      [-1, 2], 
+      [1, -2], 
+      [1, 2], 
+      [-2, -1], 
+      [-2, 1], 
+      [2, -1], 
+      [2, 1]
+    ]
     move_equations.each do |equation|
       pos_x = pos[0]
       pos_y = pos[1]
       eq_x = equation[0]
       eq_y = equation[1]
 
-      if (pos_x + eq_x <= 7 && pos_x + eq_x >= 0) && (pos_y + eq_y <= 7 && pox_y + eq_y >= 0)
-        valid_moves_arr << [pos_x + eq_x, pos_y + eq_y]
+      new_x = pos_x + eq_x
+      new_y = pos_y + eq_y 
+
+
+      if (new_x <= 7 && new_x >= 0) && (new_y <= 7 && new_y >= 0)
+        valid_moves_arr << [new_x, new_y]
       end
+      @considered_positions += valid_moves_arr
       valid_moves_arr
     end
   end
 
   def new_move_positions(pos)
-    if !@considered_positions.include?(pos)
-      if valid_moves(pos)
-        @considered_positions << pos
-        return pos
-      end
-    end
+    @considered_positions
+    # valid_moves(pos)
+    # valid_moves.each do |move|
+    #   if !@considered_positions.include?(move)
+    #     @considered_positions << move
+    #   end
+    # end
   end
 
   def build_move_tree
